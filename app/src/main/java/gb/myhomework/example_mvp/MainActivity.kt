bae6beefa4abd8ac1075bebc5242b4de3911fcfa
AlexEvdokimovSpb <3416216@gmail.com>
2021-02-27 22:53:40 +0300

    HW1 1. Удалил из presenter R.id.btnCounter 2. Выплнил небольшой рефакторинг

--- app/src/main/java/gb/myhomework/example_mvp/MainActivity.kt
+++ app/src/main/java/gb/myhomework/example_mvp/MainActivity.kt
@@ -4,34 +4,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import gb.myhomework.example_mvp.databinding.ActivityMainBinding
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.presenter.MainPresenter
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view.MainView
+import gb.myhomework.example_mvp.mvp.presenter.MainPresenter
+import gb.myhomework.example_mvp.mvp.view.MainView
 
 class MainActivity : AppCompatActivity(), MainView {
 
-    private var vb: ActivityMainBinding? = null
-
-    val presenter = MainPresenter(this)
+    private val vb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val presenter = MainPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        vb = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(vb?.root)
+        setContentView(vb.root)
 
-        val listener = View.OnClickListener {
-            presenter.counterClick(it.id)
+        fun listener(id: Int) = View.OnClickListener {
+            presenter.counterClick(id)
         }
 
-        vb?.btnCounter1?.setOnClickListener(listener)
-        vb?.btnCounter2?.setOnClickListener(listener)
-        vb?.btnCounter3?.setOnClickListener(listener)
+        vb.btnCounter1.setOnClickListener(listener(BUTTON_ONE))
+        vb.btnCounter2.setOnClickListener(listener(BUTTON_TWO))
+        vb.btnCounter3.setOnClickListener(listener(BUTTON_THREE))
     }
 
-    override fun setButtonText(index: Int, text: String) {
-        when(index){
-            0 -> vb?.btnCounter1?.text = text
-            1 -> vb?.btnCounter2?.text = text
-            2 -> vb?.btnCounter3?.text = text
-        }
+    override fun setOneButtonText(text: String) {
+        vb.btnCounter1.text = text
+    }
+
+    override fun setTwoButtonText(text: String) {
+        vb.btnCounter2.text = text
+    }
+
+    override fun setThreeButtonText(text: String) {
+        vb.btnCounter3.text = text
+    }
+
+    companion object {
+        private const val BUTTON_ONE = 0
+        private const val BUTTON_TWO = 1
+        private const val BUTTON_THREE = 2
     }
 }
--- app/src/main/java/gb/myhomework/example_mvp/mvp/presenter/MainPresenter.kt
+++ app/src/main/java/gb/myhomework/example_mvp/mvp/presenter/MainPresenter.kt
@@ -1,26 +1,31 @@
-package ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.presenter
+package gb.myhomework.example_mvp.mvp.presenter
 
-import gb.myhomework.example_mvp.R
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.model.CountersModel
-import ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view.MainView
+import gb.myhomework.example_mvp.mvp.view.MainView
+import gb.myhomework.example_mvp.mvp.model.CountersModel
 
-class MainPresenter(val mainView: MainView) {
-    val model = CountersModel()
+class MainPresenter(private val mainView: MainView) {
+    private val model = CountersModel()
 
-    fun counterClick(id: Int){
-        when(id){
-            R.id.btn_counter1 -> {
+    fun counterClick(id: Int) {
+        when (id) {
+            BUTTON_ONE -> {
                 val nextValue = model.next(0)
-                mainView.setButtonText(0, nextValue.toString())
+                mainView.setOneButtonText(nextValue.toString())
             }
-            R.id.btn_counter2 -> {
+            BUTTON_TWO -> {
                 val nextValue = model.next(1)
-                mainView.setButtonText(1, nextValue.toString())
+                mainView.setTwoButtonText(nextValue.toString())
             }
-            R.id.btn_counter3 -> {
+            BUTTON_THREE -> {
                 val nextValue = model.next(2)
-                mainView.setButtonText(2, nextValue.toString())
+                mainView.setThreeButtonText(nextValue.toString())
             }
         }
+    }
+
+    companion object {
+        private const val BUTTON_ONE = 0
+        private const val BUTTON_TWO = 1
+        private const val BUTTON_THREE = 2
     }
 }
